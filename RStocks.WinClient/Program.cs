7c067bbe00541be2fa7a16578044b48d2e99242c
Stavserge <stiring@mail.ru>
2014-09-14 11:24:52 +0400

    fix structure

---
+++ RStocks.WinClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RStocks.Model.Factories;
+using RStocks.XMLStorage;
+
+namespace RStocks.WinClient
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+
+        public static IRepositoryFactory RepositoryFactory = new XmlRepositoryFactory();
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);            
+            Application.Run(new Form1());
+        }
+    }
+}
